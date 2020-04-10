fbe9e5acfc272e64f4088eb0601f70da6a0cadf6
Valentina Casteline <valentinacasteline@Valentinas-MacBook-Pro-2.local>
2020-04-10 13:20:37 -0500

    fixed home screen for mobile, added individual portfolio pages

--- jsfile.js
+++ jsfile.js
@@ -5,3 +5,20 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+window.onscroll = function() {myStick()};
+
+// Get the navbar
+var navbar = document.getElementsByClassName("topnav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myStick() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
